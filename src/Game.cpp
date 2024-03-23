9efc6bce51d628b0d332ae24e5a4695d78633833
jroelle <ngloukik@gmail.com>
2024-03-23 18:25:56 +0300

    removed std::atomic for now

--- src/Game.cpp
+++ src/Game.cpp
@@ -5,40 +5,6 @@
 #include "UI.h"
 #include "Utility.hpp"
 
-namespace
-{
-	using PtrUI = std::shared_ptr<UI>;
-	using AtomicUI = std::atomic<PtrUI>;
-
-	template<typename T>
-	inline utility::atomic_accessor<T> access(std::atomic<std::shared_ptr<T>>& atomic)
-	{
-		return utility::atomic_accessor<T>(atomic);
-	}
-
-	void renderingThread(AtomicUI& ui)
-	{
-		if (auto accessor = access(ui))
-		{
-			accessor.get()->GetWindow().setVerticalSyncEnabled(true);
-			accessor.get()->GetWindow().setActive(true);
-		}
-
-		while (auto accessor = access(ui))
-		{
-			if (!accessor.get()->GetWindow().isOpen())
-			{
-				accessor.set_changed(false);
-				break;
-			}
-
-			accessor.get()->GetWindow().clear();
-			accessor.get()->Update();
-			accessor.get()->GetWindow().display();
-		}
-	}
-}
-
 Game::Game()
 	: _context(std::make_shared<Context>(2))
 {
@@ -51,21 +17,10 @@ std::shared_ptr<Context> Game::GetContext() const
 
 void Game::Run()
 {
-	auto ui = AtomicUI(std::make_shared<UI>("durak", 500, 500));
-	if (auto accessor = access(ui))
-		accessor.get()->GetWindow().setActive(false);
-
-	std::thread render(&renderingThread, ui);
-
-	while (auto accessor = access(ui))
+	auto ui = std::make_shared<UI>("durak", 500, 500);
+	while (ui->GetWindow().isOpen())
 	{
-		auto& window = accessor.get()->GetWindow();
-		if (!window.isOpen())
-		{
-			accessor.set_changed(false);
-			break;
-		}
-
+		auto& window = ui->GetWindow();
 		for (auto event = sf::Event{}; window.pollEvent(event);)
 		{
 			if (event.type == sf::Event::Closed)
@@ -75,14 +30,14 @@ void Game::Run()
 		}
 
 		auto round = std::make_unique<Round>(GetContext());
-		round->AddObserver(accessor.get());
+		round->AddObserver(ui);
 		while (round)
 		{
 			round = round->Run();
 		}
 
-		//window.clear();
-		//ui->Update();
-		//window.display();
+		window.clear();
+		ui->Update();
+		window.display();
 	}
 }
